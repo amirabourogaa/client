4b539997134bbd122e41d6afe1c538739f20ec65
amira <amirabourogaa0@gmail.com>
2021-05-10 05:31:19 +0200

    Preview changed

--- Screens/Preview.js
+++ Screens/Preview.js
@@ -1,10 +1,9 @@
 
 import React from 'react'
-import { Text, SafeAreaView, StyleSheet, View, ImageBackground   } from 'react-native'
+import { Text, SafeAreaView, StyleSheet, View } from 'react-native'
 import Slider from '@react-native-community/slider';
 import {  Input, Item} from 'native-base';
 import {TouchableRipple} from 'react-native-paper';
-import bgOrange from '../images/bgOrange.jpg';
 
 
 export default function Preview({ navigation }) {
@@ -12,52 +11,52 @@ export default function Preview({ navigation }) {
       
         <SafeAreaView style = {styles.container}>
              <Text style = {styles.quest}>
-                 Quel est le nombre d'exercices que vous avez fait comparé au nombre des exercices demandés 
+                 Combien de répititions avez-vous fait ?
             </Text>
            
             
-           <Item style= {{marginBottom:20, marginTop:20, backgroundColor : '#ffd4b1'}} rounded>
+           <Item style= {{marginBottom:20, marginTop:20, backgroundColor : '#ffd4b1', height:40}} rounded>
               <Input placeholder=''/>
           </Item>
 
             <Text style = {styles.quest}>
-                Quel est l'intensité de votre douleur ? 
+                Quel est l'intensité de votre douleur ?  (De 1 à 10)
             </Text>
             
            <Slider
                 style={{width:'100%', height: 100}}
                 minimumValue={0}
                 maximumValue={10}
+                step = {1}
                 minimumTrackTintColor="#191970"
                 maximumTrackTintColor="black"
             /> 
            <Text style = {styles.quest} >
-               Quel est votre niveau de gene au quotidien ? 
+               L'exercice a été difficile d'éxécuter?
             </Text>
             
-           <Slider
-               style={{width:'100%', height: 100}}
-                minimumValue={0}
-                maximumValue={10}
-                minimumTrackTintColor="#191970"
-                maximumTrackTintColor="black"
-            /> 
+            <Item style= {{marginBottom:20, marginTop:20, backgroundColor : '#ffd4b1', height:40}} rounded>
+              <Input placeholder=''/>
+          </Item>
            <Text style = {styles.quest}>
-               Vous sentez-vous limités dans vos mouvements ? 
+               Avez-vous fait tout les exercices proposés ?
             </Text>
             
-           <Slider
-                style={{width:'100%', height: 100}}
-                minimumValue={0}
-                maximumValue={10}
-                minimumTrackTintColor="#191970"
-                maximumTrackTintColor="black"
-            />
+            <Item style= {{marginBottom:20, marginTop:20, backgroundColor : '#ffd4b1',height:40}} rounded>
+              <Input placeholder=''/>
+          </Item>
+            <Text style = {styles.quest}>
+              Ils ont été facile à faire ?
+            </Text>
+            
+            <Item style= {{marginBottom:20, marginTop:20, backgroundColor : '#ffd4b1', height:40}} rounded>
+              <Input placeholder=''/>
+          </Item>
             
             <TouchableRipple onPress={ ()=> alert ('Votre Bilan a bien été envoyé ') }>
                 <View style={{ borderRadius : 80, alignItems : 'center', justifyContent :'center',height: 40,  backgroundColor: '#ffd4b1',}}>
                    
-                    <Text style={{fontSize: 20,color: '#191970' }}>Envoyer</Text>
+                    <Text style={{fontSize: 20,color: '#191970', fontWeight:'bold' }}>Envoyer</Text>
                 </View>
             </TouchableRipple>
 
@@ -69,12 +68,13 @@ export default function Preview({ navigation }) {
 const styles = StyleSheet.create({
   
     container: {
-      marginTop: 20,
+      marginTop: 30,
       marginLeft:20,
-      marginRight:20
+      marginRight:20,
+    
     },
     quest : {
-        fontSize: 18,
+        fontSize: 16,
         fontWeight: 'bold'
     }
   
